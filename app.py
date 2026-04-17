9324be814b232eb3f4a8e52bcca701acc01b5d41
gryuewua <yuchenw7@nycstudents.net>
2026-04-17 09:21:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import requests
+import tkinter as tk
+
 def catfacts(amount):
-    for a in amount:
-        response = requests.get(f"https://meowfacts.herokuapp.com/?count={a}")
-        if response.status_code != 200:
-            print("Error fetching data!")
-            return None
-        fact = response.json()
-        print(fact)
+    response = requests.get(f"https://meowfacts.herokuapp.com/?count={amount}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    fact = response.json()
+    return fact
 
 print(catfacts("5"))
+
+
+
+window = tk.Tk()
+window.title("Random Cat Facts!")
+window.geometry("500x500")
+window.resizable(False, False)
+
+prompt = tk.Label(window, text="Type your message below:",
+font=("Arial", 14))
+prompt.pack(pady=10)
+
+entry = tk.Entry(window, font=("Arial", 14), width=30)
+entry.pack(pady=5)
+
+catfacts_button = tk.Button(window, text="Reverse Message!",font=("Arial", 14),command=catfacts)
+

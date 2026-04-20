6eff0e5439532e4d99394d0a43645e40d6f467c8
gryuewua <yuchenw7@nycstudents.net>
2026-04-20 08:50:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,18 @@ def catfacts(amount):
 
 print(catfacts("5"))
 
-
-
 window = tk.Tk()
 window.title("Random Cat Facts!")
 window.geometry("500x500")
 window.resizable(False, False)
 
-prompt = tk.Label(window, text="Type your message below:",
-font=("Arial", 14))
+prompt = tk.Label(window, text="Number of facts:",font=("Arial", 14))
 prompt.pack(pady=10)
 
 entry = tk.Entry(window, font=("Arial", 14), width=30)
 entry.pack(pady=5)
 
-catfacts_button = tk.Button(window, text="Reverse Message!",font=("Arial", 14),command=catfacts)
+catfacts_button = tk.Button(window, text="Learn More About Cats!",font=("Arial", 14),command=catfacts(prompt))
+catfacts_button.pack(pady=10)
 
+window.mainloop()

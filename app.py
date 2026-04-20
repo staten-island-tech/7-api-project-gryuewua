70283f8c5b59cfafb53104fefa578044f5f3f39d
gryuewua <yuchenw7@nycstudents.net>
2026-04-20 09:25:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ window.title("Random Cat Facts!")
 window.geometry("500x500")
 window.resizable(False, False)
 
-prompt = tk.Label(window, text="Number of facts:",font=("Arial", 16))
+prompt = tk.Label(window, text="Number of facts:", font=("Arial", 16))
 prompt.pack(pady=20)
 
 entry = tk.Entry(window, font=("Arial", 14), width=30)
@@ -24,13 +24,13 @@ def catfacts():
         fact = response.json()
         print(fact['data'])
 
-catfacts_button = tk.Button(window, text="Learn More About Cats!",font=("Arial", 20),command=catfacts, relief="raised")
+catfacts_button = tk.Button(window, text="Learn More About Cats!", font=("Arial", 20), command=catfacts, relief="raised")
 catfacts_button.pack(pady=100)
 
 def meow():
     print("Meow")
 
-meow_button = tk.Button(window, text = "Meow", font = ("Arial", 14), command = meow, relief = "raised")
+meow_button = tk.Button(window, text="Meow", font=("Arial", 14), command=meow, relief="raised")
 meow_button.pack(pady=20)
 
 window.mainloop()

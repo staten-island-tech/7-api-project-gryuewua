3310a15287b968cf99295ff226ee76ebff2bd482
gryuewua <yuchenw7@nycstudents.net>
2026-04-20 08:59:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 import requests
 import tkinter as tk
 
-def catfacts(amount):
-    response = requests.get(f"https://meowfacts.herokuapp.com/?count={amount}")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    fact = response.json()
-    return fact
-
-print(catfacts("5"))
-
 window = tk.Tk()
 window.title("Random Cat Facts!")
 window.geometry("500x500")
@@ -22,7 +12,16 @@ prompt.pack(pady=10)
 entry = tk.Entry(window, font=("Arial", 14), width=30)
 entry.pack(pady=5)
 
-catfacts_button = tk.Button(window, text="Learn More About Cats!",font=("Arial", 14),command=catfacts(prompt))
+def catfacts():
+    amount=entry.get()
+    response = requests.get(f"https://meowfacts.herokuapp.com/?count={amount}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    fact = response.json()
+    return fact
+
+catfacts_button = tk.Button(window, text="Learn More About Cats!",font=("Arial", 14),command=catfacts())
 catfacts_button.pack(pady=10)
 
 window.mainloop()

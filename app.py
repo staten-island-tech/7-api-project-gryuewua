988a99fd98c4004edd4180cf7133a49b0707313f
gryuewua <yuchenw7@nycstudents.net>
2026-04-20 09:14:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ window.geometry("500x500")
 window.resizable(False, False)
 
 prompt = tk.Label(window, text="Number of facts:",font=("Arial", 14))
-prompt.pack(pady=10)
+prompt.pack(pady=20)
 
 entry = tk.Entry(window, font=("Arial", 14), width=30)
-entry.pack(pady=5)
+entry.pack(pady=20)
 
 def catfacts():
     amount=entry.get()
@@ -19,9 +19,30 @@ def catfacts():
         print("Error fetching data!")
         return None
     fact = response.json()
-    return fact
+    print(fact['data'])
+    enumerate(fact['data'])
 
-catfacts_button = tk.Button(window, text="Learn More About Cats!",font=("Arial", 14),command=catfacts())
-catfacts_button.pack(pady=10)
+catfacts_button = tk.Button(window, text="Learn More About Cats!",font=("Arial", 20),command=catfacts, relief="raised")
+catfacts_button.pack(pady=100)
 
 window.mainloop()
+
+""" def say_hello():
+    print("Hello there!")
+
+window = tk.Tk()
+window.title("Button Example")
+# Create the button
+my_button = tk.Button(
+window, # parent container
+text="Say Hello", # label text
+command=say_hello, # function to call when clicked
+font=("Arial", 16), # nice big font
+bg="lightblue", # background color
+fg="black", # text color
+relief="raised", # gives it a 3D look
+padx=10, pady=5 # padding around the text
+
+)
+my_button.pack(pady=20) # place it on the window
+window.mainloop() """

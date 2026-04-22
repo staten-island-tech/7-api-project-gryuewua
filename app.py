7195d1a9913e07491ef99198aa7dbaf55a986bee
gryuewua <yuchenw7@nycstudents.net>
2026-04-22 09:24:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,20 +15,20 @@ entry.pack(pady=0)
 def catfacts():
     amount=entry.get()
     if not amount:
-        print("Enter a valid number!") 
+        prompt.config(text="Enter a valid number!") 
     else:
         response = requests.get(f"https://meowfacts.herokuapp.com/?count={amount}")
         if response.status_code != 200:
             print("Error fetching data!")
             return None
         fact = response.json()
-        print(fact['data'])
+        prompt.config(text=f"{fact['data']}")
 
 catfacts_button = tk.Button(window, text="Learn More About Cats!", font=("Arial", 20), command=catfacts, relief="raised")
 catfacts_button.pack(pady=40)
 
 def meow():
-    print("Meow")
+    prompt.config(text="Meow")
 
 meow_button = tk.Button(window, text="Meow", font=("Arial", 20), command=meow, relief="raised")
 meow_button.pack(pady=0)

1065f50d84f5d427036cb240f6256d8882e12c46
gryuewua <yuchenw7@nycstudents.net>
2026-04-27 09:05:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ window.title("Random Cat Facts!")
 window.geometry("1000x600")
 window.resizable(False, False)
 
-explosion = tk.PhotoImage(file="boom.png")
-boom=tk.Label(window)
-
 prompt = tk.Label(window, text="Number of facts:", font=("Arial", 20))
 prompt.pack(pady=40)
 
@@ -17,12 +14,13 @@ entry.pack(pady=0)
 
 def catfacts():
     amount=entry.get()
-    if not amount:
+    if not amount or amount != int:
         answer.config(text="Enter a valid number!") 
     else:
         response = requests.get(f"https://meowfacts.herokuapp.com/?count={amount}")
         if response.status_code != 200:
             print("Error fetching data!")
+            answer.config(text="Error fetching data!") 
             return None
         fact = response.json()
         answer.config(text=f"{fact['data']}")
@@ -39,14 +37,17 @@ meow_button.pack(pady=0)
 answer = tk.Label(window, text="", font=("Arial", 16),wraplength=900)
 answer.pack(pady=20,fill="both",expand=True)
 
+explosion = tk.PhotoImage(file="boom.png")
+
 def boom():
     answer.config(text="Bro why did you do that")
     window.after(1000, boomaction)
-    window.after(1000, window.destroy)
+    window.after(1500, window.destroy)
 
 def boomaction():
-    boom.config(image=explosion) 
+    boom=tk.Label(window)
     boom.place(x=0, y=-200)
+    boom.config(image=explosion) 
     
 selfdestruct_button = tk.Button(window, text="DO NOT PRESS", font=("Arial", 10), command=boom, relief="raised")
 selfdestruct_button.place(x=450, y=550)
